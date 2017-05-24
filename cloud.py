9319dc758f2f5659d370f0efd33e4c0e60aa9759
hooper2chile <hooper.007@gmail.com>
2017-05-23 20:25:44 -0400

    cloud reparado y listo para la raspberrypi

--- cloud.py
+++ cloud.py
@@ -7,22 +7,22 @@
 import os, sys, time, datetime
 
 TIME_SYNC = 3600 #sync for 3600 [s] = 1 [hr]
-ID = '0Bxw8dfMyxUX2MF80cFlwSFF3S1E'
+ID = '0B3jT9_WfcyT9SV8xeG1SMmdCVzA'
 
 if(sys.platform=='darwin'):
     gdrive = './gdrive-osx-x64'
     DIR1 = '/Users/hooper/Dropbox/BIOCL/biocl_system/config/'
-    DIR2 = '/Users/hooper/Dropbox/BIOCL/biocl_system/database2/'
+    DIR2 = '/Users/hooper/Dropbox/BIOCL/biocl_system/csv/'
 
 else:
     gdrive = './gdrive-linux-rpi'
     DIR1 = '/home/pi/biocl_system/config/'
-    DIR2 = '/home/pi/biocl_system/database2/'
+    DIR2 = '/home/pi/biocl_system/csv/'
 
 while True:
     hora = time.strftime("Hora=%H:%M:%S__Fecha=%d-%m-%y")
     try:
-        os.system(DIR1 + gdrive + ' sync upload ' + DIR2+'.' + ' ' + ID)
+        os.system(DIR1 + gdrive + ' sync upload ' + DIR2 + '.' + ' ' + ID)
         print 'sincronizado: ' + hora
         time.sleep(TIME_SYNC)
 

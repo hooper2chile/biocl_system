b9d6b3b02c17a56423e430fb88999957d1f9f021
hooper2chile <hooper.007@gmail.com>
2017-06-20 22:25:17 -0400

    pid html+

--- static/js/calibrar.js
+++ static/js/calibrar.js
@@ -22,7 +22,7 @@ var socket = io.connect(location.protocol + '//' +
           });//fin de la función socket.on
 
 
-          $('#med1_c').css({ 'color': 'lightred', 'font-size': '100%' });
+          $('#med1_c').css({ 'color': 'green', 'font-size': '100%' });
           $('#med2_c').css({ 'color': 'red', 'font-size': '100%' });
           $('#med3_c').css({ 'color': 'red', 'font-size': '100%' });
 

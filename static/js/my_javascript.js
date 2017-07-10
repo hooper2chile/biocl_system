41a4f22da2217f7298078c826d559279e2239c1b
hooper2chile <hooper.007@gmail.com>
2017-07-10 13:19:44 -0400

    depurando database.py

--- static/js/my_javascript.js
+++ static/js/my_javascript.js
@@ -108,7 +108,7 @@ $(document).ready(function() {
     //para ser desplegados en todos los clientes.
     socket.on('power', function(msg) {
 
-        $('#select').text( msg.set[0] ).html();
+        $('#select[name=selection]').text( msg.set[0] ).html();
         //document.getElementById('checked').checked = msg.set[1];
     });
 
